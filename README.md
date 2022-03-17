a06af5d9eae1122dab96bc36983dcf34a84eb2e0
g.ferrer <gustavo.ferrer@totvs.com.br>
2022-03-16 21:23:57 -0300

    finish angle

---
+++ README.md
@@ -0,0 +1,14 @@
+# Circles Fiap
+
+## Execução
+<code>python3 circle.py</code>
+
+## Tarefas
+
+- ✅ Segmentar apenas os círculos maiores da imagem, use a técnica que achar melhor. E exibe o resultado
+- ✅ Exibe na imagem a área e o centro de massa dos dois círculos segmentados da imagem. O resultado esperado e uma imagem com um marcação no centro indicando o CM e o valor da área
+- ✅ Traça uma reta entre os dois centros dos círculos 
+- ✅ Calcula o ângulo de inclinação da reta em relação ao plano horizontal, inclinação em graus. O resultado esperado é uma imagem com  uma reta entre centros e valor do ângulo exibido na tela
+
+## Resultado
+<img src="./captura.png">
--- circle.py
+++ circle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 
+from black import out
 import cv2
 import numpy as np
+import math
 
 rval = True
 
@@ -18,11 +20,31 @@ while rval:
     if circles is not None:
         circles = np.round(circles[0, :]).astype("int")
 
+        XrightCircle = circles[0][0]; 
+        YrightCircle = circles[0][1]; 
+        RrightCircle = circles[0][2]; 
+
+        XleftCircle = circles[1][0]; 
+        YleftCircle = circles[1][1]; 
+        RleftCircle = circles[1][2]; 
+
         for (x, y, r) in circles:
             circleArea = 2 * (3.14 * r)
             circleAreaFormatted = "A: "+"{:.2f}".format(round(circleArea, 2))
 
-            if (r != 84):
+            angle = int(math.degrees(math.atan2(YleftCircle - YrightCircle,XleftCircle - XrightCircle))) * -1
+            angleFormatted = "{}{}".format("Angulo:", angle)
+
+            textsize = cv2.getTextSize(angleFormatted, font, 1, 2)[0]
+            textX = (output.shape[1] - textsize[0]) / 2
+            textY = (output.shape[0] + textsize[1]) / 2
+
+
+            isACircle = r > 100
+
+            if (isACircle):
+                cv2.putText(output, angleFormatted, (int(textX), int(textY - 100)), font, 1, (0, 0, 0), 2)
+                cv2.line(output, (XrightCircle, YrightCircle), (XleftCircle, YleftCircle), (255, 0, 0), 4)
                 cv2.circle(output, (x, y), r, (0, 255, 0), 4)
                 cv2.putText(output, circleAreaFormatted, (x - 75,y - 40), font, 1, (0, 0, 0))
                 cv2.putText(output, "CM", (x - 10, y - 10), font, .5, (0, 0, 0))
